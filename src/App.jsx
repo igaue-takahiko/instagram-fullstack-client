326ea98619fe7457a3fc5ad4e44c2687f62ffa27
igaue-takahhiko <hasewo.2610@gmail.com>
2021-02-06 16:25:52 +0900

    started templete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          hello world
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
